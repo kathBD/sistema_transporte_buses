1aa52c591a7a7bc9e6d1769aa8fbb670c9c01b76
katherine Bonilla <felicidadkg@gmail.com>
2023-02-02 13:43:57 -0500

    first commit

---
+++ src/main/java/com/sistema/sistematransporte/SistemaTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.sistematransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/sistematransporte/SistemaTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.sistematransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
